ea8e5aa2dc18c5e4e88f7b6f1a001228a9b775f8
alive-hamster <andrew@nxweb.com>
2024-06-27 23:08:12 -0400

    UV v2

--- index.js
+++ index.js
@@ -1,43 +1,38 @@
+import { createBareServer } from "@tomphttp/bare-server-node";
 import express from "express";
 import { createServer } from "node:http";
 import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
-import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
-import { baremuxPath } from "@mercuryworkshop/bare-mux";
 import { hostname } from "node:os";
-import wisp from "wisp-server-node"
 
+const bare = createBareServer("/bare/");
 const app = express();
-// Load our publicPath first and prioritize it over UV.
+
 app.use(express.static("./public"));
-// Load vendor files last.
-// The vendor's uv.config.js won't conflict with our uv.config.js inside the publicPath directory.
 app.use("/uv/", express.static(uvPath));
-app.use("/epoxy/", express.static(epoxyPath));
-app.use("/baremux/", express.static(baremuxPath));
 
 // Error for everything else
-app.use((req, res) => {
-  res.status(404);
-  res.send("404")
+app.get('*', function(req, res) {
+  res.send('404');
 });
 
+const port = 8080;
 const server = createServer();
 
 server.on("request", (req, res) => {
-  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-  app(req, res);
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
 });
+
 server.on("upgrade", (req, socket, head) => {
-  if (req.url.endsWith("/wisp/"))
-    wisp.routeRequest(req, socket, head);
-  else
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
+  } else {
     socket.end();
+  }
 });
-
-let port = parseInt(process.env.PORT || "");
-
-if (isNaN(port)) port = 8080;
 
 server.on("listening", () => {
   const address = server.address();
@@ -48,7 +43,8 @@ server.on("listening", () => {
   console.log(`\thttp://localhost:${address.port}`);
   console.log(`\thttp://${hostname()}:${address.port}`);
   console.log(
-    `\thttp://${address.family === "IPv6" ? `[${address.address}]` : address.address
+    `\thttp://${
+      address.family === "IPv6" ? `[${address.address}]` : address.address
     }:${address.port}`
   );
 });
@@ -60,6 +56,7 @@ process.on("SIGTERM", shutdown);
 function shutdown() {
   console.log("SIGTERM signal received: closing HTTP server");
   server.close();
+  bare.close();
   process.exit(0);
 }
 
